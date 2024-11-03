37f16fef96ed82cdf06a9ddb5cea12eab599845b
Bobik58 <loshininc@gmail.com>
2024-11-03 18:25:58 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
+
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+
+        internal class One : IMenuItem
+        {
+            [DllImport("user32.dll", CharSet = CharSet.Auto)]
+            public static extern int MessageBox(IntPtr hWnd, String text, String caption, int options);
+
+            public string Name => "One";
+            public void Execute()
+            {
+                MessageBox(IntPtr.Zero, "Hello, World!", "", 0);
+            }
+        }
+        internal class Two : IMenuItem
+        {
+            [DllImport("user32.dll", CharSet = CharSet.Auto)]
+            public static extern int MessageBox(IntPtr hWnd, String text, String caption, int options);
+
+            public string Name => "Two";
+            public void Execute()
+            {
+                do
+                {
+                    PlayGame();
+                } while (AskToPlayAgain());
+            }
+            static void PlayGame()
+            {
+                int min = 0;
+                int max = 100;
+                int guess;
+                bool guessedCorrectly = false;
+
+                MessageBox(IntPtr.Zero, "Загадайте число от 0 до 100 и нажмите OK.", "Игра: Угадай число", 0);
+
+                while (!guessedCorrectly)
+                {
+                    guess = (min + max) / 2;
+                    int result = MessageBox(IntPtr.Zero, $"Ваше число: {guess}. Это ваше число?", "Игра: Угадай число", 1 | 32 | 0x40000);
+
+                    if (result == 1)
+                    {
+                        MessageBox(IntPtr.Zero, $"Ура! Я угадал ваше число: {guess}!", "Игра: Угадай число", 0);
+                        guessedCorrectly = true;
+                    }
+                    else if (result == 2) 
+                    {
+                        int hintResult = MessageBox(IntPtr.Zero, $"Ваше число больше {guess}?", "Игра: Угадай число", 1 | 32 | 0x40000);
+                        if (hintResult == 1)
+                        {
+                            min = guess + 1; 
+                        }
+                        else
+                        {
+                            max = guess - 1;
+                        }
+                    }
+
+                    if (min > max)
+                    {
+                        MessageBox(IntPtr.Zero, "Похоже, вы ошиблись! Попробуйте снова.", "Игра: Угадай число", 0);
+                        return;
+                    }
+                }
+            }
+
+            static bool AskToPlayAgain()
+            {
+                    int result = MessageBox(IntPtr.Zero, "Хотите сыграть снова?", "Игра: Угадай число", 1 | 32);
+                    return result == 1;
+            }
+        }
+
+        internal class Three : IMenuItem
+        {
+            [DllImport("user32.dll", SetLastError = true)]
+            private static extern bool EnumWindows(EnumWindowsProc lpEnumFunc, IntPtr lParam);
+
+            [DllImport("user32.dll")]
+            private static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+            [DllImport("user32.dll", SetLastError = true)]
+            private static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+            [DllImport("user32.dll")]
+            private static extern bool IsWindowVisible(IntPtr hWnd);
+
+            private delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
+
+            private const uint WM_CLOSE = 0x0010;
+
+            public string Name => "Three";
+            public void Execute()
+            {
+                EnumWindows(new EnumWindowsProc(EnumWindowCallback), IntPtr.Zero);
+            }
+
+            private static bool EnumWindowCallback(IntPtr hWnd, IntPtr lParam)
+            {
+                if (IsWindowVisible(hWnd))
+                {
+                    StringBuilder windowText = new StringBuilder(256);
+                    GetWindowText(hWnd, windowText, windowText.Capacity);
+
+                    if (windowText.ToString().Contains("Блокнот") || windowText.ToString().Contains("Безымянный - Блокнот"))
+                    {
+                        SendMessage(hWnd, WM_CLOSE, IntPtr.Zero, IntPtr.Zero);
+                        Console.WriteLine($"Закрыто окно: {windowText}");
+                    }
+                }
+                return true;
+            }
+        }
+
+
+
+
+
+        static void Main()
+        {
+            var Menu = new Menu("Menu", new List<IMenuItem>
+            {
+                new One(),
+                new Two(),
+                new Three(),
+
+            });
+
+            Menu.Execute();
+        }
+    }
+}
---
+++ interfeis.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+        public interface IMenuItem
+        {
+            string Name { get; }
+            void Execute();
+        }
+
+        public class Menu : IMenuItem
+        {
+            public string Name { get; }
+            private readonly List<IMenuItem> _menuItems;
+
+            public Menu(string name, List<IMenuItem> menuItems)
+            {
+                Name = name;
+                _menuItems = menuItems;
+            }
+
+            public void Execute()
+            {
+                ShowMenu(_menuItems);
+            }
+
+            private static void ShowMenu(List<IMenuItem> menuItems)
+            {
+                int selectedIndex = 0;
+                int maxLength = menuItems.Max(item => item.Name.Length) + 4;
+
+                while (true)
+                {
+                    Console.Clear();
+                    Console.WriteLine("+" + new string('-', maxLength) + "+");
+                    for (int i = 0; i < menuItems.Count; i++)
+                    {
+                        string prefix = (i == selectedIndex) ? "* " : "  ";
+                        Console.WriteLine($"| {prefix}{menuItems[i].Name.PadRight(maxLength - 4)} |");
+                    }
+                    Console.WriteLine("+" + new string('-', maxLength) + "+");
+                    Console.WriteLine("Используйте стрелки для навигации, Enter для выбора. Esc для возврата.");
+
+                    var key = Console.ReadKey(intercept: true).Key;
+                    if (key == ConsoleKey.UpArrow)
+                    {
+                        selectedIndex = (selectedIndex > 0) ? selectedIndex - 1 : menuItems.Count - 1;
+                    }
+                    else if (key == ConsoleKey.DownArrow)
+                    {
+                        selectedIndex = (selectedIndex < menuItems.Count - 1) ? selectedIndex + 1 : 0;
+                    }
+                    else if (key == ConsoleKey.Enter)
+                    {
+                        Console.Clear();
+                        menuItems[selectedIndex].Execute();
+                        Console.WriteLine("\nНажмите любую клавишу, чтобы вернуться в меню...");
+                        Console.ReadKey();
+                    }
+                    else if (key == ConsoleKey.Escape)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+}
